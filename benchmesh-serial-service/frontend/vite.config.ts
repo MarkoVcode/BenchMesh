b3680f1e1a08131c81ff8a5105e82162768daaee
MarkoV <marek.kaszewski@gmail.com>
2025-10-09 17:48:45 +0100

    fixing FE root folder

--- benchmesh-serial-service/frontend/vite.config.ts
+++ benchmesh-serial-service/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // Use a dev server port unlikely to clash and allow any host (for container use)
 export default defineConfig({
   plugins: [react()],
+  base: '/ui/',
   server: {
     host: true,
     port: 52893,
